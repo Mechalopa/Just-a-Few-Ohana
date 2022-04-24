102a5ca51f8dff35db5b08cbc585485cd9f4e475
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2022-04-25 00:19:15 +0900

    Update

---
+++ src/main/java/com/github/mechalopa/jafohana/util/ModTags.java
@@ -0,0 +1,32 @@
+package com.github.mechalopa.jafohana.util;
+
+public class ModTags
+{
+//	public static final ITag.INamedTag<Block> RED_SPIDER_LILY_PLANTABLE_ON = makeBlockTag("red_spider_lily_plantable_on");
+//
+//	private static ITag.INamedTag<Block> makeBlockTag(String name)
+//	{
+//		return BlockTags.bind(JAFOhana.MODID + ":" + name);
+//	}
+//
+//	public static <T> boolean checkTagContains(ITag<T> tag, T value)
+//	{
+//		if (tag != null && value != null)
+//		{
+//			try
+//			{
+//				if (tag.contains(value))
+//				{
+//					return true;
+//				}
+//			}
+//			catch (Exception e)
+//			{
+//				JAFOhana.LOGGER.warn("Broken tags -> " + tag.toString(), (Throwable)e);
+//				return false;
+//			}
+//		}
+//
+//		return false;
+//	}
+}
