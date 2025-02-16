53342fe84b1d2dbe8be157cefbee49554cd9a4ae
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-02-16 18:22:33 +0900

    Follows 1.20.1 update

---
+++ src/main/java/com/github/mechalopa/jafohana/world/level/block/CreepansyBlock.java
@@ -0,0 +1,23 @@
+package com.github.mechalopa.jafohana.world.level.block;
+
+import com.github.mechalopa.jafohana.util.ModTags;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class CreepansyBlock extends ModFlowerBlock
+{
+	public CreepansyBlock(Holder<MobEffect> effect, float seconds)
+	{
+		super(effect, seconds);
+	}
+
+	@Override
+	protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos)
+	{
+		return state.is(ModTags.BlockTags.CREEPANSY_PLANTABLE_ON);
+	}
+}
