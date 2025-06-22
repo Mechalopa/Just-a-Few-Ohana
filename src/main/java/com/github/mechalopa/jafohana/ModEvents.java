3ed6e26234aac5c85f8486254880e55e30877267
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-06-22 15:40:24 +0900

    Fix particles not appearing when fasciate

--- src/main/java/com/github/mechalopa/jafohana/ModEvents.java
+++ src/main/java/com/github/mechalopa/jafohana/ModEvents.java
@@ -9,14 +9,17 @@ import com.github.mechalopa.jafohana.util.ModUtils;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
@@ -34,42 +37,47 @@ public class ModEvents
 	@SubscribeEvent
 	public static void onBonemeal(BonemealEvent event)
 	{
-		if (!event.isCanceled() && !event.getLevel().isClientSide() && event.getLevel() instanceof ServerLevel && event.getState() != null)
+		if (!event.isCanceled() && event.getState() != null)
 		{
-			ServerLevel serverLevel = (ServerLevel)event.getLevel();
+			Level level = event.getLevel();
 			BlockPos pos = event.getPos();
-			RandomSource r = serverLevel.getRandom();
+			RandomSource r = level.getRandom();
 
-			if (ModConfigs.cachedServer.DANDELION_FASCIATION_CHANCE > 0.0D && event.getState().is(ModTags.BlockTags.CONVERTABLE_TO_FASCIATED_DANDELION) && fasciate(serverLevel, pos, ModBlocks.FASCIATED_DANDELION.get().defaultBlockState(), r, ModConfigs.cachedServer.DANDELION_FASCIATION_CHANCE))
+			if (ModConfigs.cachedServer.DANDELION_FASCIATION_CHANCE > 0.0D && event.getState().is(ModTags.BlockTags.CONVERTABLE_TO_FASCIATED_DANDELION) && fasciate(level, pos, event.getState(), ModBlocks.FASCIATED_DANDELION.get().defaultBlockState(), r, ModConfigs.cachedServer.DANDELION_FASCIATION_CHANCE))
 			{
 				event.setSuccessful(true);
-				ModUtils.shrink(event.getStack(), serverLevel, event.getPlayer());
+				ModUtils.shrink(event.getStack(), level, event.getPlayer());
 			}
-			else if (ModConfigs.cachedServer.OXEYE_DAISY_FASCIATION_CHANCE > 0.0D && event.getState().is(ModTags.BlockTags.CONVERTABLE_TO_FASCIATED_OXEYE_DAISY) && fasciate(serverLevel, pos, ModBlocks.FASCIATED_OXEYE_DAISY.get().defaultBlockState(), r, ModConfigs.cachedServer.OXEYE_DAISY_FASCIATION_CHANCE))
+			else if (ModConfigs.cachedServer.OXEYE_DAISY_FASCIATION_CHANCE > 0.0D && event.getState().is(ModTags.BlockTags.CONVERTABLE_TO_FASCIATED_OXEYE_DAISY) && fasciate(level, pos, event.getState(), ModBlocks.FASCIATED_OXEYE_DAISY.get().defaultBlockState(), r, ModConfigs.cachedServer.OXEYE_DAISY_FASCIATION_CHANCE))
 			{
 				event.setSuccessful(true);
-				ModUtils.shrink(event.getStack(), serverLevel, event.getPlayer());
+				ModUtils.shrink(event.getStack(), level, event.getPlayer());
 			}
 		}
 	}
 
-	private static boolean fasciate(ServerLevel serverLevel, BlockPos blockpos, BlockState fasciatedFlowerState, RandomSource random, double chance)
+	private static boolean fasciate(Level level, BlockPos blockpos, BlockState baseFlowerState, BlockState fasciatedFlowerState, RandomSource random, double chance)
 	{
-		if (fasciatedFlowerState.canSurvive(serverLevel, blockpos) && serverLevel.isEmptyBlock(blockpos.above()))
+		if (fasciatedFlowerState.canSurvive(level, blockpos) && level.isEmptyBlock(blockpos.above()))
 		{
 			for (Direction direction : Direction.values())
 			{
 				if (direction.getAxis().isHorizontal())
 				{
-					BlockState state = serverLevel.getBlockState(blockpos.relative(direction));
+					BlockState state = level.getBlockState(blockpos.relative(direction));
 
 					if (state != null && state.is(ModTags.BlockTags.AFFECTS_FASCIATIONS))
 					{
-						if (random.nextDouble() < chance)
+						if (!(baseFlowerState.getBlock() instanceof BonemealableBlock bonemealableBlock && bonemealableBlock.isBonemealSuccess(level, level.getRandom(), blockpos, level.getBlockState(blockpos))))
+						{
+							ParticleUtils.spawnParticleInBlock(level, blockpos, 15, ParticleTypes.HAPPY_VILLAGER);
+						}
+
+						if (!level.isClientSide() && level instanceof ServerLevel && random.nextDouble() < chance)
 						{
 							BlockPos blockpos1 = blockpos.above();
-							serverLevel.setBlockAndUpdate(blockpos, DoublePlantBlock.copyWaterloggedFrom(serverLevel, blockpos, fasciatedFlowerState.setValue(DoublePlantBlock.HALF, DoubleBlockHalf.LOWER)));
-							serverLevel.setBlockAndUpdate(blockpos1, DoublePlantBlock.copyWaterloggedFrom(serverLevel, blockpos1, fasciatedFlowerState.setValue(DoublePlantBlock.HALF, DoubleBlockHalf.UPPER)));
+							level.setBlockAndUpdate(blockpos, DoublePlantBlock.copyWaterloggedFrom(level, blockpos, fasciatedFlowerState.setValue(DoublePlantBlock.HALF, DoubleBlockHalf.LOWER)));
+							level.setBlockAndUpdate(blockpos1, DoublePlantBlock.copyWaterloggedFrom(level, blockpos1, fasciatedFlowerState.setValue(DoublePlantBlock.HALF, DoubleBlockHalf.UPPER)));
 						}
 
 						return true;
--- src/main/java/com/github/mechalopa/jafohana/util/ModUtils.java
+++ src/main/java/com/github/mechalopa/jafohana/util/ModUtils.java
@@ -6,12 +6,13 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class ModUtils
 {
-	public static void shrink(ItemStack stack, ServerLevel serverLevel, @Nullable LivingEntity livingEntity)
+	public static void shrink(ItemStack stack, Level level, @Nullable LivingEntity livingEntity)
 	{
-		if (stack != null && !stack.isEmpty() && stack.getItem() != null)
+		if (stack != null && !stack.isEmpty() && stack.getItem() != null && level instanceof ServerLevel)
 		{
 			if (!stack.isDamageableItem())
 			{
@@ -24,7 +25,7 @@ public class ModUtils
 			}
 			else
 			{
-				stack.hurtAndBreak(1, serverLevel, null, p -> {});
+				stack.hurtAndBreak(1, (ServerLevel)level, null, p -> {});
 			}
 		}
 	}
