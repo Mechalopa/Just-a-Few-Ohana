e7d53f3651e4465a3d285763f6649d337dcf355d
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2025-06-22 15:41:43 +0900

    Nya

--- src/main/java/com/github/mechalopa/jafohana/ModEvents.java
+++ src/main/java/com/github/mechalopa/jafohana/ModEvents.java
@@ -68,7 +68,7 @@ public class ModEvents
 
 					if (state != null && state.is(ModTags.BlockTags.AFFECTS_FASCIATIONS))
 					{
-						if (!(baseFlowerState.getBlock() instanceof BonemealableBlock bonemealableBlock && bonemealableBlock.isBonemealSuccess(level, level.getRandom(), blockpos, level.getBlockState(blockpos))))
+						if (!(baseFlowerState.getBlock() instanceof BonemealableBlock bonemealableBlock && bonemealableBlock.isBonemealSuccess(level, random, blockpos, level.getBlockState(blockpos))))
 						{
 							ParticleUtils.spawnParticleInBlock(level, blockpos, 15, ParticleTypes.HAPPY_VILLAGER);
 						}
