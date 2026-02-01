be5ff90a2c2f56d35bf13b07134f153a4342cfb3
Mechalopa <92443696+Mechalopa@users.noreply.github.com>
2026-02-01 12:08:36 +0900

    Remove wither rose bush ;P

--- src/main/java/com/github/mechalopa/jafohana/registry/ModBlocks.java
+++ src/main/java/com/github/mechalopa/jafohana/registry/ModBlocks.java
@@ -18,7 +18,6 @@ import com.github.mechalopa.jafohana.world.level.block.ModFlowerPotBlock;
 import com.github.mechalopa.jafohana.world.level.block.ModTallFlowerBlock;
 import com.github.mechalopa.jafohana.world.level.block.RedSpiderLilyBlock;
 import com.github.mechalopa.jafohana.world.level.block.VineFlowerBlock;
-import com.github.mechalopa.jafohana.world.level.block.WitherRoseBushBlock;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.effect.MobEffects;
@@ -94,7 +93,6 @@ public class ModBlocks
 	public static final Supplier<Block> PRINCES_PLUME = REGISTRY.register("princes_plume", () -> new BadlandsTallFlowerBlock());
 	public static final Supplier<Block> FASCIATED_DANDELION = REGISTRY.register("fasciated_dandelion", () -> new FasciatedDandelionBlock());
 	public static final Supplier<Block> FASCIATED_OXEYE_DAISY = REGISTRY.register("fasciated_oxeye_daisy", () -> new FasciatedOxeyeDaisyBlock());
-	public static final Supplier<Block> WITHER_ROSE_BUSH = REGISTRY.register("wither_rose_bush", () -> new WitherRoseBushBlock());
 	public static final Supplier<Block> JADE_VINE = REGISTRY.register("jade_vine", () -> VineFlowerBlock.create());
 	public static final Supplier<Block> MYSORE_TRUMPETVINE = REGISTRY.register("mysore_trumpetvine", () -> VineFlowerBlock.create());
 	public static final Supplier<Block> POTTED_DAYFLOWER = REGISTRY.register("potted_dayflower", () -> ModFlowerPotBlock.create(DAYFLOWER.get()));
--- src/main/java/com/github/mechalopa/jafohana/registry/ModItems.java
+++ src/main/java/com/github/mechalopa/jafohana/registry/ModItems.java
@@ -8,7 +8,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.DoubleHighBlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -81,7 +80,6 @@ public class ModItems
 	public static final Supplier<Item> PRINCES_PLUME = REGISTRY.register("princes_plume", () -> createDoubleHighBlockItem(ModBlocks.PRINCES_PLUME.get()));
 	public static final Supplier<Item> FASCIATED_DANDELION = REGISTRY.register("fasciated_dandelion", () -> createDoubleHighBlockItem(ModBlocks.FASCIATED_DANDELION.get()));
 	public static final Supplier<Item> FASCIATED_OXEYE_DAISY = REGISTRY.register("fasciated_oxeye_daisy", () -> createDoubleHighBlockItem(ModBlocks.FASCIATED_OXEYE_DAISY.get()));
-	public static final Supplier<Item> WITHER_ROSE_BUSH = REGISTRY.register("wither_rose_bush", () -> createDoubleHighBlockItem(ModBlocks.WITHER_ROSE_BUSH.get(), Rarity.UNCOMMON));
 	public static final Supplier<Item> JADE_VINE = REGISTRY.register("jade_vine", () -> createBlockItem(ModBlocks.JADE_VINE.get()));
 	public static final Supplier<Item> MYSORE_TRUMPETVINE = REGISTRY.register("mysore_trumpetvine", () -> createBlockItem(ModBlocks.MYSORE_TRUMPETVINE.get()));
 
@@ -99,11 +97,6 @@ public class ModItems
 	private static DoubleHighBlockItem createDoubleHighBlockItem(Block block)
 	{
 		return new DoubleHighBlockItem(block, new Item.Properties());
-	}
-
-	private static DoubleHighBlockItem createDoubleHighBlockItem(Block block, Rarity rarity)
-	{
-		return new DoubleHighBlockItem(block, new Item.Properties().rarity(rarity));
 	}
 
 	public static DeferredRegister<Item> getItemRegistry()
